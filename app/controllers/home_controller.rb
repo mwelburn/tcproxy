2e810c5ed42f0779fe4665402775f26f23c7f145
br0nc080 <mwelbu2@gmail.com>
2013-01-24 14:47:44 -0600

    code

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,25 @@
+class HomeController < ApplicationController
+  
+	def index
+		src = "";
+		code = "";
+		campaignId = "";
+		params.each do |key, value|
+			Rails.logger.warn "Param #{key}: #{value}"
+
+			if key == "src"
+				src = value
+			elsif key == "code"
+				code = value
+			elsif key == "campaignId"
+				campaignId = value
+			end
+		end
+		url = "#{src}?code=#{code}&campaignId=#{campaignId}"
+		Rails.logger.debug "URL: #{url}"
+
+		response = HTTParty.get(url)
+		Rails.logger.debug "Response: #{response}"
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tcproxy::Application.config.secret_token = '74e5ad3d1e518f4dc74137fa5f2949dbc6c83073bc782b521cf0baf3f1f42fee4999e4d9b5151ce17a1ecdaca299b5744130503ca6bcc4bebeb3cdc3ad89175a'
